126b92b57c7961666035079f6feb5353cdc54eb2
santos <hubermonika1@t-online.de>
2026-06-26 23:15:05 +0200

    Fix Wayland and use x11 as fallback

--- billance-wrapper.sh
+++ billance-wrapper.sh
@@ -2,4 +2,8 @@
 
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
 
+if [ -n "$WAYLAND_DISPLAY" ]; then
+    set -- --ozone-platform=wayland --enable-features=UseOzonePlatform "$@"
+fi
+
 exec zypak-wrapper /app/main/billance "$@"
